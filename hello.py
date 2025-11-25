913367f32207cf2abcac5ca51fa6f5b64a4a2d33
julig17 <greif.julia.jg@gmail.com>
2025-11-25 10:33:17 +0100

    Hello World

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hallo Python')
+l = [1,2,3]
+for ele in l:
+    print(ele)
+
+zahl1 = int(input("Zahl 1 eingeben:"))
+zahl2 = int(input("Zahl 2 eingeben:"))
+ergebnis = (zahl1 + zahl2) * 3
+print ("(", zahl1, "+", zahl2, ")", "* 3 =", ergebnis)
