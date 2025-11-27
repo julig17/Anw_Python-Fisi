a9a0346644c1edbbaee96524210d0b0b3efbd936
julig17 <greif.julia.jg@gmail.com>
2025-11-27 11:22:57 +0100

    Kommentare hinzugefügt

--- sprachelemente/zahlen.py
+++ sprachelemente/zahlen.py
@@ -1,6 +1,12 @@
+#Darstellung der Dezimal 19 in Hexadezamial
+#Zur Kennzeichnungvon hexadezimal-Darstellung verwendet python  #0x
 print(hex(19))
 #0x13
+#Darstellung der Dezimal 19 in Oktal
+#Zur Kennzeichnungvon Oktal-Darstellung verwendet python  #0o
 print(oct(19))
 #0o230
+#Darstellung der Dezimal 19 in Binär
+#Zur Kennzeichnungvon Binär-Darstellung verwendet python  #0b
 print(bin(19))
-#0b
+#0b10011
