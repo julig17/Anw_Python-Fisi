1d7f8146d50fde212876a996f3e5bf619f6c7d86
julig17 <greif.julia.jg@gmail.com>
2025-12-01 09:31:12 +0100

    Aufgabe Grundelegende Sprachelemente

---
+++ sprachelemente/bezahlen.py
@@ -0,0 +1,12 @@
+anzahl = 6
+preis = 13.5
+vorname = "Julia"
+
+print(vorname,"kauft",anzahl,"Artikel zum Einzelpreis von",
+       preis,"Euro und bezahlt:",anzahl * preis)
+
+#Mischung von Datentypen int und str, muss beim Zusammensetzen in str
+#umgewandelt werden
+print(vorname + " kauft " + str(anzahl) + " Artikel zum Einzelpreis von "
+       + str(preis) + " Euro und bezahlt: " + str(anzahl * preis))
+
