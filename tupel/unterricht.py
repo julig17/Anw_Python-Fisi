a489ed530e6c33d9e794dd8395d3124c10868a04
julig17 <greif.julia.jg@gmail.com>
2026-01-11 14:37:51 +0100

    Unterricht Tupel

---
+++ tupel/unterricht.py
@@ -0,0 +1,14 @@
+#Definition
+mein_tupel = ("Apfel", "Apfel", "Banane", "Kirsche")
+print(mein_tupel)
+zahlen_tupel = (1,2,2,5,15)
+print(zahlen_tupel)
+#gleiche Elemente sind erlaubt
+doppel_tupel = ("Apfel", "Apfel", "Banane", "Kirsche")
+print(doppel_tupel)
+#gemischte Datentypen-Elemente sind erlaubt
+gemischtes_tupel = ("Apfel", 5, 5.5, "Kirsche")
+print(gemischtes_tupel)
+#Tupel können wiederum Tupel enthalten
+mehrdimensionales_tupel = ("Apfel", (1,2,3), "Kirsche")
+print(mehrdimensionales_tupel)
