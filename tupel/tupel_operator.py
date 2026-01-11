f00db826c910df9a1043e1ab7277d38522e5bf1e
julig17 <greif.julia.jg@gmail.com>
2026-01-11 15:18:00 +0100

    Aufgabe: Tupel Operatoren

---
+++ tupel/tupel_operator.py
@@ -0,0 +1,17 @@
+"""Füge zwei vorher festgelegte Tupel zu einer zusammen und gib diese aus:
+
+Beispiel: (2,6,9) zusammengefügt mit (1,4,6) ergibt (2,6,9,1,4,6)
+Gib vom Tupel das 2 bis 4 Element aus.
+Was passiert wenn du versuchst ein Element mit einem Index auszugeben, das über die Anzahl der Elemente im Tupel hinausgeht?
+Prüfe mit Operator in / not in ob Elemente im Tupel drin sind, oder nicht
+Lösche das letzte Element aus dem Tupel."""
+
+tupel_1 = (2,6,9)
+tupel_2 = (1,4,6)  
+tupel_3 = tupel_1 + tupel_2
+print(tupel_3)
+print(tupel_3[1:4])
+# print(tupel_3[10])  # IndexError: tuple index out of range
+print(2 in tupel_3)
+print(10 not in tupel_3)
+# del tupel_3[-1]  # TypeError: 'tuple' object doesn't support item deletion
