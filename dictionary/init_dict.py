20dba8972d033878f890e45a5d9c71ccecc618de
julig17 <greif.julia.jg@gmail.com>
2026-01-21 18:24:58 +0100

    Anlegen eines Wörterbuchs

---
+++ dictionary/init_dict.py
@@ -0,0 +1,9 @@
+#Anlegen eines deutsch englisch Wörterbuchs über input in while Schleife
+woerterbuch = {}
+while True:
+    deutsch = input("Geben Sie ein deutsches Wort ein (oder 'ende' zum Beenden): ")
+    if deutsch.lower() == 'ende':
+        break
+    englisch = input("Geben Sie die englische Übersetzung ein: ")
+    woerterbuch[deutsch] = englisch 
+print("Wörterbuch:", woerterbuch)   
