471c7905e83603c8a9c5898a3a8e2b0555e833f2
julig17 <greif.julia.jg@gmail.com>
2026-01-30 10:30:58 +0100

    Opertoren von der Menge

--- set/operatoren.py
+++ set/operatoren.py
@@ -1,10 +1,19 @@
 menge1 = {1, 2, 3, 4, 5}
 menge2 = {4, 5}
-print(f"1 in Menge {menge1=}: {1 in menge1}")
-print(f"1 nicht in Menge {menge1=}: {1 not in menge1}")
 print(f"{menge2=} Teilmenge von {menge1=}: {menge2 <= menge1}")
 print(f"{menge2=} Teilmenge von {menge1=}: {menge2.issubset(menge1)}")
+
 print(f"{menge1=} Obermenge von {menge2=}: {menge1 >= menge2}")
-print(f"Vereinigungsmenge von {menge1=} und {menge2=} ist {menge1 | menge2}")
+print(f"{menge1=} Obermenge von {menge2=}: {menge1.issuperset(menge2)}")
+
+menge3 = {7, 8}
+print(f"Vereinigungsmenge von {menge1=} und {menge3=} ist {menge1 | menge3}")
+vereinigungsmenge = menge1 | menge3
+print(vereinigungsmenge)
+print(f"Vereinigungsmenge von {menge1=} und {menge3=} ist {menge1.union(menge3)}")
+
 print(f"Schnittmenge von {menge1=} und {menge2=} ist {menge1 & menge2}")
+print(f"Schnittmenge von {menge1=} und {menge3=} ist {menge1 & menge3}")
+
 print(f"Differenzmenge von {menge1=} und {menge2=} ist {menge1 - menge2}")
+print(f"Differenzmenge von {menge2=} und {menge1=} ist {menge2 - menge1}")
