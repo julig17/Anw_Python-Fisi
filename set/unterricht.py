0aa4ab9712da3eb1cf0bd7b0921d975e4a2a0cd3
julig17 <greif.julia.jg@gmail.com>
2026-01-30 10:52:03 +0100

    Unterrichtsstoff Set 30.01.2026

--- set/unterricht.py
+++ set/unterricht.py
@@ -1,65 +1,35 @@
-#anlegen einer leeren Set
-leeres_set = set()
-print("Leeres Set:", type(leeres_set))
+dict = {"one" : 1, "two" : 2}
+leere_set = {1,2,3,5,3,4,5}
+#print(dict)
+print(leere_set)
 
-#anlegen eines Sets mit Werten
-zahlen_set = {1, 2, 3, 4, 5}    
-print("Zahlen Set:", zahlen_set)
-
-#gleiche Werte werden im Set nur einmal gespeichert
-duplikate_set = {1, 2, 2, 3, 4, 4, 5}
-print("Set mit Duplikaten (werden entfernt):", duplikate_set)
-
-#set mit unterschiedlichen Datentypen
-gemischtes_set = {1, "Hallo", 3.14, True}
-print("Gemischtes Set:", gemischtes_set)
-
-#Kein Indexzugriff bei Sets möglich
 try:
-    print(gemischtes_set[0])
-except TypeError as e:
-    print("Fehler beim Zugriff per Index auf ein Set:", e)
+    gemischte_set = { 1, "Hallo", 3.14, [1,2]}
+except TypeError:
+    print("TypeError wegen falscher Datentypen")
+except Exception as fehler:
+    print("Fehler", fehler)
+
+#print("Gemischte Set", gemischte_set)
+
+zahlen_set = {1,2,3,4,5}
+zahlen_set.add(7)
+print(zahlen_set)
+
+zahlen_frozen = frozenset([1,2,3,4,5])
+#zahlen_frozen.add(8)
 
 
-#set mit mutierbaren Elementen (Listen) ist nicht erlaubt
-try:
-    ungueltiges_set = {1, 2, [3, 4]}
-except TypeError as e:
-    print("Fehler beim Erstellen eines Sets mit Listen:", e)
+#print(dict["one"])
+#print(zahlen_set[0])
 
-
-#immutable Set
-immutable_set = frozenset([1, 2, 3, 4, 5])
-print("Immutable Set (frozenset):", immutable_set)
-print("Typ des immutable Sets:", type(immutable_set))
-try:
-    immutable_set.add(6)
-except AttributeError as e:
-    print("Fehler beim Hinzufügen zu einem frozenset:", e)
-
-#Set aus anderen Datentypen erstellen
-string_set = set("Hallo Welt")
-print("Set aus String:", string_set)
-liste_set = set([1, 2, 3, 4, 5])
-print("Set aus Liste:", liste_set)
-print("Set aus Tupel:", set((1, 2, 3, 4, 5)))
-print("Set aus Bereich (range):", set(range(1, 6)))
-dict_set = set({'a': 1, 'b': 2, 'c': 3})
-print("Set aus Dictionary (nur Schlüssel):", dict_set)
-
-
-#Kein Indexzugriff bei Sets möglich
-zahlen_set = {1, 2, 3, 4, 5}
-try:
-    print(zahlen_set[0])
-except TypeError as e:
-    print("Fehler beim Zugriff per Index auf ein Set:", e)
-#Zugriff auf Set-Elemente durch Iteration
-print("Iterieren über ein Set:")    
 for element in zahlen_set:
-    print(element)  
-#Überprüfen auf Mitgliedschaft
-print("Ist 3 im zahlen_set?", 3 in zahlen_set)
-#set mit len()
-print("Länge des zahlen_set:", len(zahlen_set)) 
+    print(element)
 
+print(5 in zahlen_set)
+print(20 in zahlen_set)
+print(20 not in zahlen_set)
+print(5 not in zahlen_set)
+print(len(zahlen_set))
+
+auto_set ={"BMW", "Opel"}
