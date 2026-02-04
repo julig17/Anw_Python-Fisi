3fd244e3bba5c9a5b19a6adc9ef218ab88d965ff
julig17 <greif.julia.jg@gmail.com>
2026-02-04 16:35:14 +0100

    JSON Daten parsen und generieren

---
+++ json/parse_konfig.py
@@ -0,0 +1,14 @@
+import json
+
+try:        
+    with open("./json/konfig.json", "r", encoding="UTF-8") as konfig_datei:
+# json-Datei komplett in die Liste personen Liste einlesen
+        konfig = json.load(konfig_datei)
+except FileNotFoundError as e:
+    print("Datei nicht gefunden", e)
+except Exception as e:
+    print(f"Sonstiger Dateizugriffsfehler: {e}")
+
+print(konfig["hostname"])
+print(konfig["ip"])
+print(konfig["port"])
---
+++ json/parse_personen.py
@@ -0,0 +1,33 @@
+#Modul json muss eingebunden werden
+import json
+
+personen_liste = []
+
+try:        
+    with open("./json/personen.json", "r", encoding="UTF-8") as personendatei:
+# json-Datei komplett in die Liste personen Liste einlesen
+        personen_liste = json.load(personendatei)
+except FileNotFoundError as e:
+    print("Datei nicht gefunden", e)
+except Exception as e:
+    print(f"Sonstiger Dateizugriffsfehler: {e}")
+
+print("So sieht der geparste JSON String aus")
+print(personen_liste)
+print("-------------------")
+
+#da unsere Daten in einer Liste liegen, können wir durch die liste iterieren 
+#und die Personen ausgeben
+for person in personen_liste:
+    #jetzt geben wir die Person aus, aus dict
+    for eigenschaft, wert in person.items():
+        if eigenschaft == "kinder":
+            print(f"{eigenschaft} :")
+            for kind in wert:
+                for eigenschaft, wert in kind.items():
+                    print(f"\t{eigenschaft} : {wert}")
+                print("\t---------------")
+        else:
+            print(f"{eigenschaft} : {wert}")
+    print("-------------------")
+
