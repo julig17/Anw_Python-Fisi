b777cc9729b39d649206886fa077734eb08cd82a
julig17 <greif.julia.jg@gmail.com>
2026-02-08 20:52:41 +0100

    Exceptions, Unterricht Vorbereitung

---
+++ exceptions/fehlerarten.py
@@ -0,0 +1,22 @@
+#print(x)
+# NameError: name 'x' is not defined
+
+#"5" + 3
+#TypeError: can only concatenate str (not "int") to str
+
+#int("abc")
+#ValueError: invalid literal for int() with base 10: 'abc'
+
+#liste = [1, 2, 3]
+#print(liste[5])
+#IndexError: list index out of range
+
+#data = {"name": "Max"}
+#print(data["alter"])
+#KeyError: 'alter'
+
+#10 / 0
+#ZeroDivisionError: division by zero
+
+#open("nicht_da.txt", "r")
+#FileNotFoundError: [Errno 2] No such file or directory: 'nicht_da.txt' 
