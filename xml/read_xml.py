571d971e567d08b00fd4d912c513f746ef53c460
julig17 <greif.julia.jg@gmail.com>
2026-02-25 08:36:04 +0100

    XML Parsen

--- xml/read_xml.py
+++ xml/read_xml.py
@@ -1,40 +1,19 @@
 import xml.etree.ElementTree as ElementTree
 
-DATEINAME = "./xml/data.xml"
-TYPEN = {
-    "int" : int,
-    "str" : str
-    }
+dateiname = "./xml/data.xml"
 
-"""
-Funktion liest aus schluessel- oder wert-Tags das Attribut typ aus
- und konvertiert den  Text in den durch typ angegebenen Datentyp.
-"""
-def lese_element(element):
-    #Attribut typ wird ausgelesen 
-    typ = element.get("typ", "str")
-    try:
-        #Wert in richtigen Datentp wird zurückgegeben z.B int("15")
-        return TYPEN[typ](element.text)
-    except KeyError:
-        return element.text
+baum = ElementTree.parse(dateiname) 
+#hole Wurzelelement
+tag_dict = baum.getroot()
 
-""" Haupfunktion bekommt den Dateinamen übergeben, gibt den Datendict mit
-Daten aus dem XML Dokument zurück"""
-def lade_dict(dateiname): 
-    d = {}
-    #XML Daten werden zu einem Baum aufbearbeitet
-    baum = ElementTree.parse(dateiname) 
-    #hole Wurzelelement
-    tag_dict = baum.getroot()
-    #laufe durch Kindelemente
-    for eintrag in tag_dict:
-        tag_schluessel = eintrag.find("schluessel")
-        tag_wert = eintrag.find("wert")
-        schluessel = lese_element(tag_schluessel)
-        wert = lese_element(tag_wert) 
-        d[schluessel] = wert 
-    return d
+print(tag_dict)
 
-daten_im_dict = lade_dict(DATEINAME)
-print(daten_im_dict)
+for kind_element in tag_dict:
+    print("Kindelemente: ", kind_element)
+    schluessel = kind_element.find("schluessel")
+    #Inhalt vom Tag   
+    tag_inhalt = schluessel.text
+    #tag Name
+    print(f"Tag: {schluessel.tag} : {tag_inhalt} ")
+    #Attribut vom Tag
+    print(f"Tag: {schluessel.get("typ")} ")
