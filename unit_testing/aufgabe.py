6c9eb4ddc983347041eaa808bf9c84182085af07
julig17 <greif.julia.jg@gmail.com>
2026-03-05 17:00:31 +0100

    versankosten

--- unit_testing/aufgabe.py
+++ unit_testing/aufgabe.py
@@ -5,3 +5,15 @@ def maximum_von_zwei_zahlen(zahl1, zahl2):
     return max(zahl1, zahl2)
 
 
+def versandkosten(gewicht):
+    if gewicht < 0:
+        raise ValueError("Gewicht darf nicht negativ sein")
+
+    if gewicht < 1:
+        return 2.99
+    elif gewicht < 5:
+        return 4.99
+    else:
+        return 7.99
+
+
